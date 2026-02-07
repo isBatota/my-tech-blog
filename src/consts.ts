c1af6e36e2f1831e747607416085f6e619e70f2f
isBatota <BenaliAli4321@gmail.com>
2026-02-07 20:20:55 +0100

    fix: change the content of pages : about/contact/home/privacy

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Batota';
+export const SITE_DESCRIPTION = 'A mix of programming, IT, and a long journey through bugs, errors, and bad decisions.';
